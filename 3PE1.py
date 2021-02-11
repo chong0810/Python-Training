92ef850b84a1c4779fc12771dd01f09c23a9bd8d
Daniel Hong <hongchangyeon@gmail.com>
2021-02-10 19:33:01 -0600

    3PE1 exercise finished

---
+++ 3PE1.py
@@ -0,0 +1,19 @@
+chooser = int(input("Pick a number between 1 - 7"))
+
+if (chooser == 1):
+    print("Monday")
+elif (chooser == 2):
+    print("Tuesday")
+elif (chooser == 3):
+    print("Wednesday")
+elif (chooser == 4):
+    print("Thursday")
+elif (chooser == 5):
+    print("Friday")
+elif (chooser == 6):
+    print("Saturday")
+elif (chooser == 7):
+    print("Sunday")
+else:
+    print("Wrong input. Exiting Program.")
+
