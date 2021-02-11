bac518f904083b08cc0d329bac3371e92004a5c6
Daniel Hong <hongchangyeon@gmail.com>
2021-02-10 19:50:16 -0600

    added pseudo code

--- 3PE1.py
+++ 3PE1.py
@@ -1,3 +1,17 @@
+# Program: Pick A Day
+# Description: This program will determine the day from your input from 1 to 7
+#   
+# Author: Daniel Hong 
+# Date: 2/10/21
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+
+# Declare constants (name in ALL_CAPS)
+
+# Declare Variable types (EVERY variable used)
+
 chooser = int(input("Pick a number between 1 - 7"))
 
 if (chooser == 1):
@@ -17,3 +31,4 @@ elif (chooser == 7):
 else:
     print("Wrong input. Exiting Program.")
 
+# End Program
