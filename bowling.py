30f97a809254b9bb4fec8297faf27186897444b9
Daniel Hong <hongchangyeon@gmail.com>
2021-02-12 15:55:56 -0600

    simple bowling simulation

--- bowling.py
+++ bowling.py
@@ -27,25 +27,341 @@ frameTotal = 0
 gameTotal = 0
 
 #Bowling Game Simulation
-#Inital Print
-print("Frame    Ball 1    Ball2   Frame Total     Game Score")
-
 #Frame Number Updater 1
 
-frameNumber = int(input("What is the current frame number?"))
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 1
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 1
+frameTotal = 0
 
 # Number of pins knocked down by the ball one 1
 
-ball_1 = int(input("How many pins were knocked down by the first ball?"))
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
 
 gameTotal = (gameTotal + ball_1)
 
 # Number of pins knocked down by the ball two 1
 
-ball_2 = int(input("How many pins were knocked down by the second ball?"))
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
 
-gameTotal = ()
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 1
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 1
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 1
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 2
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 2
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 2
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 2
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 2
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 2
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 3
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 3
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 3
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 3
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 3
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 3
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 3
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
 
 
+#Frame Number Updater 4
 
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 4
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 4
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 4
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 4
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 4
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 4
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 5
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 5
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 5
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 5
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 5
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 5
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 5
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 6
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 6
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 6
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 6
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 6
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 6
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 6
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 7
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 7
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 7
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 7
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 7
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 7
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 7
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 8
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 8
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 8
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 8
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 8
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 8
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 8
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 9
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 9
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 9
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 9
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 9
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 9
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 9
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+#Frame Number Updater 10
+
+frameNumber = int(input("What is the current frame number?   "))
+
+# Reset the Ball Totals 10
+ball_1 = 0
+ball_2 = 0
+
+# Reset frame total 10
+frameTotal = 0
+
+# Number of pins knocked down by the ball one 10
+
+ball_1 = int(input("How many pins were knocked down by the first ball?   "))
+
+gameTotal = (gameTotal + ball_1)
+
+# Number of pins knocked down by the ball two 10
+
+ball_2 = int(input("How many pins were knocked down by the second ball?  "))
+
+gameTotal = (gameTotal + ball_2)
+
+# Number of pins knocked down on current frame 10
+
+frameTotal = (ball_1 + ball_2)
+
+# Displaying Current Frame 10
+# header
+print("Frame    Ball 1    Ball 2   Frame Total     Game Score")
+#printing Score
+print("{0}        {1}         {2}        {3}               {4}".format(frameNumber, ball_1, ball_2, frameTotal, gameTotal))
+
+
+#End of the Simulation
+print ("Your total Score is = {0}".format(gameTotal))
+print ("Exiting the bowling simulation")
 # End Program
+
+
