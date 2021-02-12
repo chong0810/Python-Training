d71be58e4241240ee98e46f26523551e0fc0fb08
Daniel Hong <hongchangyeon@gmail.com>
2021-02-12 15:13:30 -0600

    pythong HW1

---
+++ bowling.py
@@ -0,0 +1,34 @@
+# Program: Bowling
+# Description: This program is a bowling simulation program which it will take user's input to calculate and display the score.
+#   
+# Author: Daniel Hong 
+# Date: 2/12/2021
+# Revised: # 
+
+# list libraries used
+
+# Declare constants (name in ALL_CAPS)
+
+# Declare Variable types (EVERY variable used)
+
+# Frame number
+frameNumber = 0
+
+# Ball 1 
+ball_1 = 0
+
+# Ball 2
+ball_2 = 0
+
+# Frame Total
+frameTotal = 0
+
+# Game Score
+gameScore = 0
+
+#Simulation
+
+#Inital Print
+print("Frame    Ball 1    Ball2   Frame Total     Game Score")
+
+# End Program
