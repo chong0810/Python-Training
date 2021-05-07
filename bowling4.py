b594b679b16fa497a3c7c80da95eb6f3cca1df58
ChangYeon Hong <hongchangyeon@gmail.com>
2021-05-06 21:59:00 -0500

    Python Bowling assignment 3 setup

---
+++ bowling4.py
@@ -0,0 +1,88 @@
+# Program Bowling Simulation
+# Description: This program is a bowling simulation. It takes user's input, calculates the total and displays the score.
+# 
+# Author: Chang Yeon Hong
+# Date: May 6, 2021
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+
+# Declare global constants (name in ALL_CAPS)
+
+NUMBER_OF_FRAMES = int(10)
+NUMBER_OF_PINS = int(10)
+
+
+def main():
+
+    # Declare Variable types (EVERY variable used in this main program)
+    
+
+
+    play_game ()
+    roll_frame ()
+    roll_ball ()
+    print_the_game ()
+
+
+    
+def play_game():
+
+    # Declare Local Variable types (NOT parameters)
+
+    print ( "play_game" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} Function stub()
+
+def roll_frame ():
+
+    # Declare Local Variable types (NOT parameters)
+
+    print ( "roll_frame" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} Function stub()
+
+def roll_ball ():
+
+    # Declare Local Variable types (NOT parameters)
+
+    print ( "roll_ball" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} Function stub()
+
+def print_the_game ():
+
+    # Declare Local Variable types (NOT parameters)
+
+    print ( "print_the_game" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} Function stub()
+
+
+
+
+
+
+
+
+
+# End Program
+
+
+
+main()
+
+
