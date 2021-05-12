73af728a552b72313d0317560b2435dc2344750e
ChangYeon Hong <hongchangyeon@gmail.com>
2021-05-12 15:47:55 -0500

    user account creation

--- individual_project.py
+++ individual_project.py
@@ -1,22 +1,53 @@
-# Program <program name> 
-# Description: 
-#   <what the program does> 
-# Author: <your name> 
-# Date: <today’s date>
+# Program: Basic Login
+# Description: This program will  
+# Author: Chang Yeon Hong
+# Date: 12 May 2021
 # Revised: 
 #   <revision date> 
 
 # list libraries used
-
+import individual_project_functions
 # Declare global constants (name in ALL_CAPS)
 
 
 def main():
-
+    
     # Declare Variable types (EVERY variable used in this main program)
+
+    userInput = str()
+    checker = True
     
     
+    print("""Welcome to ACC Hospital Check-in Portal.\nPlease select from following options.\n
+    1. Check-in\n
+    2. Find my ID\n
+    3. Exit\n """)
+
+    userInput = str(input("Enter the number: "))
+
+    while userInput != "1" and userInput != "2" and userInput !="3":
+        userInput = str(input("Please, enter the correct number: "))
+    # } end while
+
+    if userInput == "1":
+        userName = input("Enter your user name: ")
+        checker = individual_project_functions.checkUserAccount(userName)
+        if checker == True:
+            print("Thanks for checking in!")
+        # } end if
+        else:
+            print("Exiting program")
+        # } end else
+
+    elif userInput == "2":
+        
+
+
+
 
 # End Program
+
+
+
 
 main()
---
+++ individual_project_functions.py
@@ -0,0 +1,88 @@
+# Program: Basic Login Functions
+# Description: This is a list of functions that will be used for checking in the patient.
+# Author: Chang Yeon Hong
+# Date: 12 May 2021
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+import random
+# Declare global constants (name in ALL_CAPS)
+
+# Function checkUserAccount()
+# Description: This will check if the user already has an account
+# Calls:
+#   none
+# Parameters:
+#   none
+# Returns:
+#   none
+
+def checkUserAccount (userID):
+
+    # Declare Local Variable types (NOT parameters)
+
+
+
+    print ( "checkUserAccount" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} end Function createUserAccount()
+
+# Function createUserAccount()
+# Description: This will check if the user already has an account
+# Calls:
+#   none
+# Parameters:
+#   none
+# Returns:
+#   none
+
+def createUserAccount ():
+
+    # Declare Local Variable types (NOT parameters)
+    first_name = str()
+    last_name = str()
+    birth_date = int()
+    userID = str()
+    duplicateChecker = str()
+    file1 = str()
+    newAccount = []
+
+    try:
+        file1 = open("database.txt","r")
+
+    except:
+        print("There is no data in the database. Please create one.")
+    else:
+        duplicateChecker = file1.read()
+        file1.close()
+    
+    first_name = input("What is your First Name: ")
+    last_name = input("What is your Last Name: " )
+    birth_date = (input("When is your Birthday in MMDDYYYY format: "))
+    userID = (first_name[0]+last_name+str(birth_date)).lower()
+
+    print(duplicateChecker)
+
+    if userID in duplicateChecker:
+        print("You already have an account!")
+    else:
+        newAccount = [userID, first_name, last_name, birth_date]
+        print(newAccount)
+        file1 = open("database.txt","a")
+        file1.write(str(newAccount)+"/")
+        file1.close()
+
+
+    
+
+    print ( "createUserAccount" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} end Function createUserAccount()
+
