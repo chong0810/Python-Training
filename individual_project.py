dc71a504d5ce5ab15de134aab9f2bbfe65dd2605
ChangYeon Hong <hongchangyeon@gmail.com>
2021-05-12 09:21:20 -0500

    intial commit

---
+++ individual_project.py
@@ -0,0 +1,22 @@
+# Program <program name> 
+# Description: 
+#   <what the program does> 
+# Author: <your name> 
+# Date: <today’s date>
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+
+# Declare global constants (name in ALL_CAPS)
+
+
+def main():
+
+    # Declare Variable types (EVERY variable used in this main program)
+    
+    
+
+# End Program
+
+main()
