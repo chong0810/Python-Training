eb7ff68e14f65c06884ba28a997401f965e696a5
ChangYeon Hong <hongchangyeon@gmail.com>
2021-05-13 15:41:01 -0500

    palindrome

---
+++ palindrome/palindrome.py
@@ -0,0 +1,21 @@
+# Program Palindrome 
+# Description: This program will tell if the word or the sentence is a palindrome.
+# Author: Chang Yeon Hong
+# Date: 13 May 2021
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+import palindrome_functions
+# Declare global constants (name in ALL_CAPS)
+
+def main():
+
+# Declare Variable types (EVERY variable used in this main program)
+    palindromeString = str()
+    palindrome_functions.checkPalindrome()
+        
+
+# End Program
+
+main()
---
+++ palindrome/palindrome_functions.py
@@ -0,0 +1,33 @@
+# Program: Palindrome_functions 
+# Description: 
+#   template with a stub function
+# Author: <your name> 
+# Date: <today’s date>
+# Revised: 
+#   <revision date> 
+
+# list libraries used
+
+# Declare global constants (name in ALL_CAPS)
+
+# Function stub()
+# Description:
+#   just here as a stub: rename and rewrite for real programs
+# Calls:
+#   none
+# Parameters:
+#   none
+# Returns:
+#   none
+
+def checkPalindrome(palindrome):
+
+    # Declare Local Variable types (NOT parameters)
+
+    print ("Palindrome Functions" )  # so I can test-run the template and not get an error
+
+
+    # Return the return variable, if any
+
+#} Function stub()
+
